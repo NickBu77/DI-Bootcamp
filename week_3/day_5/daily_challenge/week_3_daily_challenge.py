c264f1b2064b897a53af82a3c5809942000c63f5
Nick Burak <n.burak77@gmail.com>
2023-12-11 01:05:41 +0200

    week3mandat.

--- week_3/day_5/daily_challenge/week_3_daily_challenge.py
+++ week_3/day_5/daily_challenge/week_3_daily_challenge.py
@@ -1,3 +1,15 @@
+# Part 1: Quiz
+
+# What is a class? A class is a blueprint that contains a number of related functions(methods) w/in it
+# What is an instance? An nstance is a specific example you run through the class. For example, a car company may have nultiple instances for every car in class Brand.
+# What is encapsulation? This creates private attributes using underscores. It is to restrict modification of the data.
+# What is abstraction? This refers to the idea that features and functions like len() in python have code that runs in the background, yet we abstract away these details and to make our lives easier use these functions to avoid having to do these functions manually using for loops, while loops, etc...
+# What is inheritance? It is when child classes take on the methods of the parent to avoid having to repeat these methods.
+# What is multiple inheritance? A child class can have two different parents. If both have the same operation, the operation from the class lower in the script will override the higher.
+# What is polymorphism? Polymorphism is when you have the same function defined in two different classes. Depending on the class specified when the instance is defined, that's the output you'll see once ran.
+# What is method resolution order or MRO? This determines the order in which methods have priorities within class families.
+
+
 # Part 2: Create A Deck Of Cards Class.
 import random
 
